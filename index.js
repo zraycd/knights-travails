f9673d32b7abec565381b822a7b8d7d7f5d7c8e6
zray <strayzray@gmail.com>
2024-09-12 11:20:53 +0300

    created getAllPos func

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ function knightMoves(start, finish) {
     [0, 0, 0, 0, 0, 0, 0, 0],
   ];
 }
+
+function getAllPos(pos, possibleMoves = []) {
+  const offsetsX = [-1, -2, -2, 1, -1, 2, 1, 2];
+  const offsetsY = [-2, -1, 1, -2, 2, -1, 2, 1];
+
+  for (let i = 0; i < 8; i++) {
+    let newX = pos[0] + offsetsX[i];
+    let newY = pos[1] + offsetsY[i];
+    if (newX >= 0 && newX <= 7 && newY >= 0 && newY <= 7) {
+      possibleMoves.push([newX, newY]);
+    }
+  }
+
+  return possibleMoves;
+}
